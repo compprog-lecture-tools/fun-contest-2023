10aee9b8167abf6e68aa8eb7c3220dc6930d6a58
Max784 <hoffmann7@uni-potsdam.de>
2023-07-05 17:11:21 +0200

    added test ases

--- cp23/hpi-fun2/alarm/executables/generator.cpp
+++ cp23/hpi-fun2/alarm/executables/generator.cpp
@@ -25,6 +25,15 @@ const string_view SAMPLE4 = R"(3
 )";
 
 
+const string_view HSAMPLE1 = R"(3
+50 8 2.5
+25 1 3
+80 3 6 
+)";
+
+const string_view HSAMPLE2 = R"(1 
+50 5 4.999999999)";
+
 
 template <class F>
 void testcase(string name, string desc, F f) {
@@ -48,10 +57,49 @@ int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
     rnd.setSeed(7368683479736286663ll);
 
+    const int MAX = 1e6;
+
     sample(1, SAMPLE1);
     sample(2, SAMPLE2);
     sample(3, SAMPLE3);
     sample(4, SAMPLE4);
+
+    testcase("possible small S shape", "possible with a S shaped path", HSAMPLE1);
+
+    testcase("possible small with double", "fails if floats are used for the radius", HSAMPLE2);
+
+    testcase("many random points", "random ranges", []{
+        cout << Max << endl;
+        rep(i, MAX){
+            int x = rnd.next(0.0, 100.0);
+            int y = rnd.next(0.0, 10.0);
+            int r = rnd.next(0.0, 0.005);
+            cout << x << " " << y << " " << r << endl;
+        }
+    });
+
+    testcase("many random points with relative high radius", "random ranges", []{
+        cout << Max << endl;
+        rep(i, MAX){
+            int x = rnd.next(0.0, 100.0);
+            int y = rnd.next(0.0, 10.0);
+            int r = rnd.next(5.0, 10.0);
+            cout << x << " " << y << " " << r << endl;
+        }
+    });
+
+    testcase("many random points with a corridor on top", "random ranges", []{
+        cout << Max << endl;
+        rep(i, MAX){
+            int x = rnd.next(0.0, 100.0);
+            int y = rnd.next(0.0, 9.5);
+            int r = rnd.next(0.0, 0.05);
+            cout << x << " " << y << " " << r << endl;
+
+        }
+    });
+
+    
 
     return 0;
 }
