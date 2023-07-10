9246667a1d4666e587a5a463eb394d0e20ef59e5
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-07-10 11:05:48 +0200

    Improve test case generation

--- cp23/hpi-fun2/cubes/executables/generator.cpp
+++ cp23/hpi-fun2/cubes/executables/generator.cpp
@@ -1,11 +1,26 @@
 #include <testlib.h>
 #include <bits/stdc++.h>
 
+#include <FastNoiseLite.h>
+
 using namespace std;
 
-const string_view SAMPLE1 = R"(2
+const long long MAX_COORDINATE = 1e12;
+const int MAX_N = 1e5;
+
+const string_view SAMPLE1 = R"(7
+2 2 2
+2 2 1
+2 2 3
+2 1 2
+2 3 2
+1 2 2
+3 2 2
+)";
+
+const string_view SAMPLE2 = R"(2
 1 1 1
-1 1 2
+1 1 3
 )";
 
 template <class F>
@@ -30,12 +45,52 @@ void sample(int num, string_view content)
     predefined("sample" + num_str, "Sample #" + num_str, content);
 }
 
+vector<array<long long, 3>> generate_coords(int width, int length, int height)
+{
+    vector<array<long long, 3>> coords;
+
+    const long long base_x_coordinate = rnd.next(MAX_COORDINATE);
+    const long long base_y_coordinate = rnd.next(MAX_COORDINATE);
+    const long long base_z_coordinate = rnd.next(MAX_COORDINATE);
+
+    // Threshold for the cloud block
+    const float THRESHOLD = 0.4;
+
+    FastNoiseLite noiseGenerator;
+
+    // Configure the noise
+    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_ValueCubic);
+    noiseGenerator.SetFrequency(0.1);
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < length; y++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                // Generate noise value
+                float noise = noiseGenerator.GetNoise((float)x, (float)y, (float)z);
+
+                // If noise value is above threshold, generate a cloud block
+                if (noise > THRESHOLD)
+                    coords.push_back({base_x_coordinate + x, base_y_coordinate + y, base_z_coordinate + z});
+
+                if (coords.size() == MAX_N)
+                    break;
+            }
+        }
+    }
+
+    return coords;
+};
+
 int main(int argc, char *argv[])
 {
     registerGen(argc, argv, 1);
     rnd.setSeed(-1143501878491944586ll);
 
     sample(1, SAMPLE1);
+    sample(2, SAMPLE2);
 
     testcase("empty", "empty", []
              { println(0); });
@@ -58,21 +113,46 @@ int main(int argc, char *argv[])
         println(1, 1, 1);
         println(1, 1, 2); });
 
-    const long long MAX_COORDINATE = 10e12;
-    const int MAX_N = 10e5;
+    testcase("diagonals", "diagonals", []
+             {
+        println(2);
+        println(1, 1, 1);
+        println(1, 2, 2); });
 
-    for (int i = 0; i < 10; i++)
+    testcase("complex-diagonals", "complex diagonals", []
+             {
+        println(6);
+        println(2, 2, 1);
+        println(2, 2, 3);
+        println(2, 1, 2);
+        println(2, 3, 2);
+        println(1, 2, 2);
+        println(3, 2, 2); });
+
+    testcase("max_coords", "max coordinates", []
+             {
+        println(2);
+        println(MAX_COORDINATE, MAX_COORDINATE, MAX_COORDINATE);
+        println(MAX_COORDINATE - 1, MAX_COORDINATE - 1, MAX_COORDINATE - 1); });
+
+    testcase("max_n", "max n", []
+             {
+    auto coords = generate_coords(1000, 1000, 1000);
+    int n = coords.size();
+    println(n);
+    for (int i = 0; i < n; i++)
+        println(coords[i][0], coords[i][1], coords[i][2]); });
+
+    for (int j = 0; j < 10; j++)
     {
-        testcase("random" + to_string(i), "random", []
+
+        testcase("random" + to_string(j), "random", []
                  {
-			int n = rnd.next(MAX_N);
+            auto coords = generate_coords(100 + rnd.next(100), 100 + rnd.next(100), 100 + rnd.next(100));
+            int n = coords.size();
             println(n);
-            for(int i = 0; i < n; i++) {
-                long long x = rnd.next(MAX_COORDINATE);
-                long long y = rnd.next(MAX_COORDINATE);
-                long long z = rnd.next(MAX_COORDINATE);
-                println(x, y, z);
-            } });
+            for(int i = 0; i < n; i++)
+                println(coords[i][0], coords[i][1], coords[i][2]); });
     }
 
     return 0;
