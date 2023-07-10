8aa753e7cb6d9af2642d9ac55b74338b743a69cd
Marcel Garus <github@marcelgarus.dev>
2023-07-10 18:48:02 +0200

    Work on stuff

--- cp23/hpi-fun2/dinetravel/executables/generator.cpp
+++ cp23/hpi-fun2/dinetravel/executables/generator.cpp
@@ -1,5 +1,5 @@
-#include <testlib.h>
 #include <bits/stdc++.h>
+#include <testlib.h>
 
 using namespace std;
 
@@ -18,101 +18,107 @@ const string_view SAMPLE2 = R"(4
 )";
 
 template <class F>
-void testcase(string name, string desc, F f)
-{
-    ofstream desc_file(name + ".desc");
-    desc_file << desc;
-    string in_file = name + ".in";
-    freopen(in_file.c_str(), "w", stdout);
-    f();
+void testcase(string name, string desc, F f) {
+  ofstream desc_file(name + ".desc");
+  desc_file << desc;
+  string in_file = name + ".in";
+  freopen(in_file.c_str(), "w", stdout);
+  f();
 }
 
-void predefined(string name, string desc, string_view content)
-{
-    testcase(name, desc, [&]()
-             { cout << content; });
+void predefined(string name, string desc, string_view content) {
+  testcase(name, desc, [&]() { cout << content; });
 }
 
-void sample(int num, string_view content)
-{
-    auto num_str = toString(num);
-    predefined("sample" + num_str, "Sample #" + num_str, content);
+void sample(int num, string_view content) {
+  auto num_str = toString(num);
+  predefined("sample" + num_str, "Sample #" + num_str, content);
 }
 
 #define MAX 1000000
 
-int main(int argc, char *argv[])
-{
-    registerGen(argc, argv, 1);
-    rnd.setSeed(553905892603446589ll);
+int main(int argc, char *argv[]) {
+  registerGen(argc, argv, 1);
+  rnd.setSeed(553905892603446589ll);
 
-    sample(1, SAMPLE1);
-    sample(2, SAMPLE2);
+  sample(1, SAMPLE1);
+  sample(2, SAMPLE2);
 
-    // testcase("big ask historians again", "big ask historians again", []
-    //          {
-    // 		int prefix = rnd.next(1, 9);
-    // 		prefix = prefix*(MAX/10) + prefix*(MAX/100);
-    // 		int l = rnd.next(MAX/100);
-    // 		int r = rnd.next(l, MAX/100);
-    // 		assert(r<=MAX/100);
-    // 		assert(l<=r);
-    // 		println(l+prefix, r+prefix); });
+  // testcase("big ask historians again", "big ask historians again", []
+  //          {
+  // 		int prefix = rnd.next(1, 9);
+  // 		prefix = prefix*(MAX/10) + prefix*(MAX/100);
+  // 		int l = rnd.next(MAX/100);
+  // 		int r = rnd.next(l, MAX/100);
+  // 		assert(r<=MAX/100);
+  // 		assert(l<=r);
+  // 		println(l+prefix, r+prefix); });
 
-    // testcase("big with many possibilities", "big with many possibilities ", []
-    //          {
-    // 		int prefix = rnd.next(1, 9);
-    // 		prefix = prefix*(MAX/10) + prefix*(MAX/100);
-    // 		int l = rnd.next(MAX/100);
-    // 		int r = rnd.next(l, MAX/100);
-    // 		assert(r<=MAX/100);
-    // 		assert(l<=r);
-    // 		println(l+prefix, r+prefix); });
+  // testcase("big with many possibilities", "big with many possibilities ", []
+  //          {
+  // 		int prefix = rnd.next(1, 9);
+  // 		prefix = prefix*(MAX/10) + prefix*(MAX/100);
+  // 		int l = rnd.next(MAX/100);
+  // 		int r = rnd.next(l, MAX/100);
+  // 		assert(r<=MAX/100);
+  // 		assert(l<=r);
+  // 		println(l+prefix, r+prefix); });
 
-    // testcase("big with few possibilities", "big with few possibilities ", []
-    //          {
-    // 		int prefix = rnd.next(1, 9);
-    // 		prefix = prefix*(MAX/10) + prefix*(MAX/100);
-    // 		int l = rnd.next(MAX/100);
-    // 		int r = rnd.next(l, MAX/100);
-    // 		assert(r<=MAX/100);
-    // 		assert(l<=r);
-    // 		println(l+prefix, r+prefix); });
+  // testcase("big with few possibilities", "big with few possibilities ", []
+  //          {
+  // 		int prefix = rnd.next(1, 9);
+  // 		prefix = prefix*(MAX/10) + prefix*(MAX/100);
+  // 		int l = rnd.next(MAX/100);
+  // 		int r = rnd.next(l, MAX/100);
+  // 		assert(r<=MAX/100);
+  // 		assert(l<=r);
+  // 		println(l+prefix, r+prefix); });
 
-    testcase("big with unconnected lasagna", "big with unconnected lasagna ", []
-             {
-                int n = MAX;
+  testcase("big-lasagna-separate", "big with unconnected lasagna", [] {
+    ll n = MAX;
 
-                ll timeline[n];
-                rep(n, i) timeline[i] = i;
-                // Fisher-Yates-shuffle, https://stackoverflow.com/questions/22850316/how-to-shuffle-elements-in-a-vector-randomly
-                rep(n, k)
-                {
-                    ll r = k + rnd.next(MAX) % (n - k);
-                    swap(timeline[k], timeline[r]);
-                }
+    vector<ll> timeline;
+    rep(n, i) timeline.push_back(i);
+    shuffle(timeline.begin(), timeline.end());
 
-                ll causes[n];
-                rep(n - 2, i) {
-                    causes[i] = i + rnd.next(n - 2 - i);
-                }
+    vector<ll> causes;
+    rep(n - 2, i) { causes.push_back(i + 1 + rnd.next(n - 2 - i)); }
 
-                vector<pair<ll, ll>> edges;
-                rep(n - 2, i) {
-                    edges.push_back({i, causes[i]});
-                }
-                rep(n, k)
-                {
-                    ll r = k + rnd.next(MAX) % (n - k);
-                    swap(edges[k], edges[r]);
-                }
+    vector<pair<ll, ll>> edges;
+    rep(n - 2, i) { edges.push_back({i, causes[i]}); }
+    shuffle(edges.begin(), edges.end());
 
-                cout << n << "\n";
-                rep(n, i)
-                    cout << timeline[i] << " \n"[i == n - 1];
-                for (auto edge : edges) {
-                    cout << edge.first << " " << edge.second << "\n";
-                } });
+    cout << n << "\n";
+    rep(n, i) cout << timeline[i] << " \n"[i == n - 1];
+    for (auto edge : edges) {
+      cout << timeline[edge.first] << " " << timeline[edge.second] << "\n";
+    }
+  });
 
-    return 0;
+  testcase("big", "big", [] {
+    ll n = MAX;
+
+    vector<ll> timeline;
+    rep(n, i) timeline.push_back(i);
+    shuffle(timeline.begin(), timeline.end());
+
+    vector<ll> causes;
+    rep(n - 1, i) { causes.push_back(i + 1 + rnd.next(n - 1 - i)); }
+
+    vector<pair<ll, ll>> edges;
+    rep(n - 1, i) { edges.push_back({i, causes[i]}); }
+    shuffle(edges.begin(), edges.end());
+    auto removed = edges[n - 2];
+    edges.pop_back();
+
+    cout << n << "\n";
+    rep(n, i) cout << timeline[i] << " \n"[i == n - 1];
+    for (auto edge : edges) {
+      cout << timeline[edge.first] << " " << timeline[edge.second] << "\n";
+    }
+
+    cout << "removed: " << timeline[removed.first] << "(" << removed.first << ") " << timeline[removed.second] << " (" << removed.second << ")\n";
+  });
+
+  return 0;
 }
--- cp23/hpi-fun2/dinetravel/executables/solution.cpp
+++ cp23/hpi-fun2/dinetravel/executables/solution.cpp
@@ -5,58 +5,56 @@ using namespace std;
 #define ll long long
 #define rep(n, i) for (ll i = 0; i < n; i++)
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    int n;
-    cin >> n;
+  ll n;
+  cin >> n;
 
-    int id_by_time[n];
-    int time_by_id[n];
-    rep(n, time)
-    {
-        int id;
-        cin >> id;
-        id_by_time[time] = id;
-        time_by_id[id] = time;
+  vector<ll> id_by_time(n);
+  vector<ll> time_by_id(n);
+  rep(n, time) {
+    ll id;
+    cin >> id;
+    id_by_time[time] = id;
+    time_by_id[id] = time;
+  }
+
+  vector<vector<ll>> graph(n);  // for each event, what caused it
+  rep(n - 2, i) {
+    ll from, to;
+    cin >> from >> to;
+    if (time_by_id[to] < time_by_id[from]) {
+      cout << "ask historians again\n";
+      return 0;
+    }
+    graph[to].push_back(from);
+  }
+
+  vector<bool> connected_to_lasagna(n, false);
+  queue<ll> qrious;  // times of events about which we are curious
+  for (qrious.push(n - 1); !qrious.empty(); qrious.pop()) {
+    auto time_of_event = qrious.front();
+    auto event = id_by_time[time_of_event];
+    connected_to_lasagna[time_of_event] = true;
+    for (auto cause : graph[event]) qrious.push(time_by_id[cause]);
+  }
+
+  // cout << "visited: ";
+  // rep(n, i) cout << (connected_to_lasagna[i] ? 'x' : '_');
+  // cout << endl;
+
+  ll last_not_connected_time;
+  for (ll i = n - 2; i >= 0; i--)
+    if (!connected_to_lasagna[i]) {
+      last_not_connected_time = i;
+      break;
     }
 
-    vector<int> graph[n]; // for each event, what caused it
-    rep(n - 2, i)
-    {
-        int from, to;
-        cin >> from >> to;
-        if (time_by_id[to] < time_by_id[from])
-        {
-            cout << "ask historians again\n";
-            return 0;
-        }
-        graph[to].push_back(from);
-    }
+  ll number_of_possibilities_for_connections = n - 1 - last_not_connected_time;
+  cout << number_of_possibilities_for_connections << "\n";
+  cout << id_by_time[last_not_connected_time] << " " << id_by_time[n - 1];
 
-    vector<bool> connected_to_lasagna(n, false);
-    queue<ll> qrious; // about which events we are curious
-    for (qrious.push(id_by_time[n - 1]); !qrious.empty(); qrious.pop())
-    {
-        auto event = qrious.front();
-        connected_to_lasagna[event] = true;
-        for (auto cause : graph[event])
-            qrious.push(cause);
-    }
-
-    int last_not_connected_event;
-    for (int i = n - 2; i >= 0; i--)
-        if (!connected_to_lasagna[i])
-        {
-            last_not_connected_event = i;
-            break;
-        }
-
-    int number_of_possibilities_for_connections = n - 1 - time_by_id[last_not_connected_event];
-    cout << number_of_possibilities_for_connections << "\n";
-    cout << last_not_connected_event << " " << id_by_time[n - 1];
-
-    return 0;
+  return 0;
 }
