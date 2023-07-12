f3fd07d6acd6bb7e1d8a93a792db8518b2878ae8
conradhalle <conradhalle@gmail.com>
2023-07-12 17:17:02 +0200

    implemented simple testcases

--- cp23/hpi-fun2/comppong/executables/generator.cpp
+++ cp23/hpi-fun2/comppong/executables/generator.cpp
@@ -3,23 +3,35 @@
 
 using namespace std;
 
-const string_view SAMPLE1 = R"(1
-5 4
+const string_view SAMPLE1 = R"(5 4
 2  3  1  4 -2
 -2  2 -4 -1  2
 -3 -5 -1  6 -5
 -1 -2  2  6  9
 )";
 
-const string_view SAMPLE2 = R"(
-5 4
+const string_view SAMPLE2 = R"(5 4
 1 2 -1 -4 -20
 -8 -3 4 2 1
 3 8 10 1 3
 -4 -1 1 7 -6
 )";
 
-template <class F> void testcase(string name, string desc, F f) {
+const string_view SINGLECUP = R"(1 1
+100
+)";
+
+const string_view ONLYOWNCUPS = R"(5 5
+2 2 2 2 2
+2 2 2 2 2
+2 2 2 2 2
+2 2 2 2 2
+2 2 2 2 2
+)";
+
+template <class F>
+void testcase(string name, string desc, F f)
+{
   ofstream desc_file(name + ".desc");
   desc_file << desc;
   string in_file = name + ".in";
@@ -27,22 +39,30 @@ template <class F> void testcase(string name, string desc, F f) {
   f();
 }
 
-void predefined(string name, string desc, string_view content) {
-  testcase(name, desc, [&]() { cout << content; });
+void predefined(string name, string desc, string_view content)
+{
+  testcase(name, desc, [&]()
+           { cout << content; });
 }
 
-void sample(int num, string_view content) {
+void sample(int num, string_view content)
+{
   auto num_str = toString(num);
   predefined("sample" + num_str, "Sample #" + num_str, content);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   registerGen(argc, argv, 1);
   rnd.setSeed(5120471233170915017ll);
 
   sample(1, SAMPLE2);
+  sample(2, SAMPLE1);
+  sample(3, SINGLECUP);
+  sample(4, ONLYOWNCUPS);
 
-  testcase("random", "random", []() {
+  testcase("random", "random", []()
+           {
     int n = 1000;
     cout << n << endl;
     for (int i = 0; i < n; i++) {
@@ -50,8 +70,7 @@ int main(int argc, char *argv[]) {
         cout << rnd.next(-10, 10) << ' ';
       }
       cout << endl;
-    }
-  });
+    } });
 
   return 0;
 }
--- cp23/hpi-fun2/comppong/executables/solution.cpp
+++ cp23/hpi-fun2/comppong/executables/solution.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
 // Implementation of Kadane's algorithm for
 // 1D array. The function returns the maximum
 // sum and stores starting and ending indexes
 // of the maximum sum subarray at addresses
 // pointed by start and finish pointers
 // respectively.
-int kadane(vector<int> &arr, int &start, int &finish, int n){
+int kadane(vector<int> &arr, int &start, int &finish, int n)
+{
     int sum = 0, maxSum = INT_MIN, i;
     finish = -1;
     int local_start = 0;
 
-    for (i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i)
+    {
         sum += arr[i];
-        if (sum < 0) {
+        if (sum < 0)
+        {
             sum = 0;
             local_start = i + 1;
         }
-        else if (sum > maxSum) {
+        else if (sum > maxSum)
+        {
             maxSum = sum;
             start = local_start;
             finish = i;
@@ -30,8 +35,10 @@ int kadane(vector<int> &arr, int &start, int &finish, int n){
     maxSum = arr[0];
     start = finish = 0;
 
-    for (i = 1; i < n; i++) {
-        if (arr[i] > maxSum) {
+    for (i = 1; i < n; i++)
+    {
+        if (arr[i] > maxSum)
+        {
             maxSum = arr[i];
             start = finish = i;
         }
@@ -41,7 +48,8 @@ int kadane(vector<int> &arr, int &start, int &finish, int n){
 
 // The main function that finds
 // maximum sum rectangle in M[][]
-void findMaxSum(vector<vector<int>> &M, ll h, ll w){
+void findMaxSum(vector<vector<int>> &M, ll h, ll w)
+{
     // Variables to store the final output
     int maxSum = INT_MIN, finalLeft, finalRight, finalTop, finalBottom;
 
@@ -49,13 +57,15 @@ void findMaxSum(vector<vector<int>> &M, ll h, ll w){
     int sum, start, finish;
 
     // Set the left column
-    for (left = 0; left < w; ++left) {
+    for (left = 0; left < w; ++left)
+    {
         // Initialize all elements of temp as 0
-        vector<int> temp (h, 0);
+        vector<int> temp(h, 0);
 
         // Set the right column for the left
         // column set by outer loop
-        for (right = left; right < w; ++right) {
+        for (right = left; right < w; ++right)
+        {
 
             // Calculate sum between current left
             // and right for every row 'i'
@@ -74,7 +84,8 @@ void findMaxSum(vector<vector<int>> &M, ll h, ll w){
             // Compare sum with maximum sum so far.
             // If sum is more, then update maxSum and
             // other output values
-            if (sum > maxSum) {
+            if (sum > maxSum)
+            {
                 maxSum = sum;
                 finalLeft = left;
                 finalRight = right;
@@ -95,14 +106,17 @@ void findMaxSum(vector<vector<int>> &M, ll h, ll w){
 // Driver Code
 int main()
 {
-    int w, h; cin >> w >> h;
+    int w, h;
+    cin >> w >> h;
     vector<vector<int>> matrix(h, vector<int>(w));
-    for (int i=0; i < h; i++){
-        for (int j=0; j< w; j++){
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
             cin >> matrix[i][j];
         }
     }
-    findMaxSum(matrix);
+    findMaxSum(matrix, h, w);
 
     return 0;
 }
