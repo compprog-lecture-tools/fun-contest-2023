897835150d5091beeb2ce618d2da16d538bd0314
roehrt <33495148+roehrt@users.noreply.github.com>
2023-07-21 22:57:23 +0200

    Cleanup solution tobi

--- cp23/hpi-fun2/roomcount/executables/solution-tobi.cpp
+++ cp23/hpi-fun2/roomcount/executables/solution-tobi.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-#define rep(i, a, b) for (ll i = a; i < (b); ++i)
+#define rep(i, b) for (ll i = 0; i < (b); ++i)
 #define all(a) a.begin(), a.end()
 #define sz(x) static_cast<ll>((x).size())
 
@@ -13,36 +13,34 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    ll n, m;
-    cin >> n >> m;
+    ll n, m; cin >> n >> m;
     vvi adj(n);
-    rep(i, 0, m) {
-        ll u, v;
-        cin >> u >> v;
+    rep(i, m) {
+        ll u, v; cin >> u >> v;
         adj[--u].push_back(--v), adj[v].push_back(u);
     }
 
-    vi seen1(n), seen2(n);
+    vi seen(n);
     ll ans = 0;
-    rep(i, 0, n) sort(all(adj[i]), [&](ll a, ll b) { return sz(adj[a]) > sz(adj[b]); });
-    rep(S, 0, n) {
-        if (seen1[S]) continue;
+    rep(i, n) sort(all(adj[i]), [&](ll a, ll b) { return sz(adj[a]) > sz(adj[b]); });
+    rep(S, n) {
+        if (seen[S]) continue;
         vi q{S};
-        seen1[S] = 1;
-        rep(j, 0, sz(q))
+        seen[S] = 1;
+        rep(j, sz(q))
             for (ll v: adj[q[j]])
-                if (!seen1[v]++) q.push_back(v);
-        rep(i, 0, n) reverse(all(adj[i]));
+                if (!seen[v]++) q.push_back(v);
+        rep(i, sz(q)) reverse(all(adj[q[i]]));
+        rep(i, sz(q)-1) seen[q[i]] = 0;
         vi evt(sz(q));
         q = {q.back()};
-        seen2[q[0]] = 1;
         ll open = 0;
-        rep(i, 0, sz(q)) {
+        rep(i, sz(q)) {
             evt[i + sz(adj[q[i]]) - open++]++;
             ans = max(ans, open);
             open -= evt[i];
             for (ll v: adj[q[i]])
-                if (!seen2[v]++) q.push_back(v);
+                if (!seen[v]++) q.push_back(v);
         }
     }
     cout << ans << endl;
