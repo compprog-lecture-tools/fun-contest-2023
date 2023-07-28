9ed31d6b7fc1f96d916b8a899a1604774a804dcb
Jonas Schmidt <mail@schmidt-jonas.de>
2023-07-27 23:45:36 -0600

    fix solution-jonas

--- cp23/hpi-fun2/whispers/executables/solution-jonas.py
+++ cp23/hpi-fun2/whispers/executables/solution-jonas.py
@@ -1,25 +1,26 @@
 c, n = map(int, input().split())
 
-adj = [[]] * n
+adj = [[] for i in range(n)] 
 for _ in range(n-1):
-    a, b, c = map(int, input().split())
+    a, b, d = map(int, input().split())
     assert(0 <= a < n and 0 <= b < n)
-    adj[a].append((b, c))
-
+    adj[a].append((b, d))
+    adj[b].append((a, d))
 
 def bfs(s):
     q = [s]
     dist = [-1] * n
     dist[s] = 0
     while len(q) > 0:
-        v = q.pop()
+        v = q[-1]
+        q.pop()
         for e in adj[v]:
             if dist[e[0]] >= 0:
                 continue
             dist[e[0]] = dist[v] + e[1]
             q.append(e[0])
     far = max(range(n), key=lambda i: dist[i])
-    assert(min(dist) == 0)
+    assert min(dist) == 0
     return far, dist[far]
 
 
