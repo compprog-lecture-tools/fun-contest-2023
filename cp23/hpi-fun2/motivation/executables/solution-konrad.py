730c51f07bd79308f6d2cfc3d490b17ad583903a
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2023-08-03 22:03:46 +0200

    Shorten solution

--- cp23/hpi-fun2/motivation/executables/solution-konrad.py
+++ cp23/hpi-fun2/motivation/executables/solution-konrad.py
@@ -1,15 +1,13 @@
 n, l = map(int, input().split())
 m = list(map(int, input().split()))
 adj = [[] for _ in range(n)]
-for subordinate, boss in enumerate([int(x) - 1 for x in input().split()], start=1):
-    m[subordinate] = m[boss] + m[subordinate] if m[boss] <= l else l + 1
-    adj[boss].append(subordinate)
-unmotivatable = []
+for s, b in enumerate([int(x) - 1 for x in input().split()], start=1):
+    m[s] = m[b] + m[s] if m[b] <= l else l + 1
+    adj[b].append(s)
+ans = 0
 dp = n * [0]
-for employee in range(n - 1, -1, -1):
-    if m[employee] > l:
-        unmotivatable.append(employee)
-        continue
-    dp[employee] = max(1, -1 + sum([dp[x] for x in adj[employee]]))
-print(dp[0], len(unmotivatable))
+for e in range(n - 1, -1, -1):
+    ans += m[e] > l
+    dp[e] = int(m[e] <= l) and max(1, -1 + sum([dp[x] for x in adj[e]]))
+print(dp[0], ans)
 
