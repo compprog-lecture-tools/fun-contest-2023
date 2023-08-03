36dfd3d452b24b536dec502e530d7e6ef55fbe72
KoKonuts <89521080+PonderKoKo@users.noreply.github.com>
2023-08-03 18:25:58 +0200

    Fix RE in IO Check

--- cp23/hpi-fun2/motivation/executables/solution-check-io.wa.py
+++ cp23/hpi-fun2/motivation/executables/solution-check-io.wa.py
@@ -1,10 +1,10 @@
 n, l = map(int, input().split())
 assert 1 <= n <= 10 ** 5
 assert 1 <= l <= 10 ** 5
-m = map(int, input().split())
+m = list(map(int, input().split()))
 assert len(m) == n
 assert all(-10 ** 6 <= x <= 10 ** 6 for x in m)
-p = map(int, input().split())
+p = list(map(int, input().split()))
 assert len(p) == n-1
 assert all(1 <= x <= i for i, x in enumerate(p, 1))
 print(-1)
